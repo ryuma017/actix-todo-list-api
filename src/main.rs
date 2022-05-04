d1ba5a68fb44f725a89e56ca75d8086f309ac1a4
ryuma017 <tgcrym01@gmail.com>
2022-05-04 18:18:22 +0900

    done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use actix_web::{self, middleware::Logger, App, HttpServer};
+use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 
-use todo_list_api::routes::health_check;
+use todo_list_api::routes::task::{
+    complete_task, fail_task, get_task, pause_task, start_task, submit_task,
+};
+use todo_list_api::repository::ddb::DDBRepository;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -8,13 +11,22 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
+    let config = aws_config::load_from_env().await;
     HttpServer::new(move || {
+        let ddb_repo: DDBRepository = DDBRepository::init(String::from("task"), config.clone());
+        let ddb_data = Data::new(ddb_repo);
         let logger = Logger::default();
-        App::new().wrap(logger).service(health_check)
+        App::new()
+            .wrap(logger)
+            .app_data(ddb_data)
+            .service(get_task)
+            .service(submit_task)
+            .service(start_task)
+            .service(complete_task)
+            .service(pause_task)
+            .service(fail_task)
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("127.0.0.1", 80))?
     .run()
-    .await?;
-
-    Ok(())
+    .await
 }
